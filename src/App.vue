df7c8a35e5912054e441994457111eca7cbdf501
sayoi341 <20alt341@gmail.com>
2021-06-20 22:21:27 +0900

    change:ディレクトリ構成を修正

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
     </div>
     <router-view/>
   </div>
@@ -26,7 +25,7 @@
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+#nav a{
   color: #42b983;
 }
 </style>
