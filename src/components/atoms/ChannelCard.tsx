c12b9114b86c0b698b6d62b05b4af3b088e4f22f
sayoi341 <20alt341@gmail.com>
2021-08-27 22:55:17 +0900

    add: テキストチャンネルの生成

--- src/components/atoms/ChannelCard.tsx
+++ src/components/atoms/ChannelCard.tsx
@@ -2,33 +2,45 @@ import type { FC } from "react";
 
 import { channelsData } from "../organisms/ChannelList";
 
-import '../../styles/components/atoms/ChannelCard.module.scss';
+import style from '../../styles/components/atoms/ChannelCard.module.scss';
 
-type Props = Pick<channelsData, 'channel_name'| 'channel_topics'| 'channel_type'| 'parent'>
+type Props = Pick<channelsData,
+  'channel_name'|
+  'channel_topics'|
+  'channel_type'|
+  'parent'
+  > & {
+  "state": string
+}
 
 
-const ChannelCard: FC<Props> = (props) => {
+const ChannelCard: FC<Props> = props => {
+  if (props.channel_type === "category") {
+    return (<>wow</>);
+  } else {
 
+    const isOpen: boolean = props.state === "open";
 
+    return (
+      <div className={style.accordion_one}>
+        <div className={`${style.accordion_header}  ${isOpen && style.open}`}>
+          <div className={style.i_box} >
+            <i className={`${style.one_i} ${style.one}`}/>
+            <i className={`${style.one_i} ${style.two}`}/>
+            <i className={`${style.one_i} ${style.three}`}/>
+            <i className={`${style.one_i} ${style.four}`}/>
+          </div>
+          {props.channel_name}
+        </div>
 
-  return (
-    <div className="accordion_one">
-      <div className="accordion_header">
-        <div className="i_box" >
-          <i className="one_i one"/>
-          <i className="one_i two"/>
-          <i className="one_i three"/>
-          <i className="one_i four"/>
+        <div className={style.accordion_inner}>
+          <div className={style.box_one}>
+            <p className={style.txt_a_ac}>aa</p>
+          </div>
         </div>
       </div>
-      {props.channel_name}
-    <div className="accordion_inner">
-      <div className="box_one">
-        <p className="txt_a_ac">aa</p>
-      </div>
-    </div>
-  </div>
-  );
+    );
+  }
 }
 
 export default ChannelCard;
--- src/components/organisms/ChannelList.tsx
+++ src/components/organisms/ChannelList.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 import ChannelCard from "../atoms/ChannelCard";
+import {useEffect, useState} from "react";
 
 
 export type channelsData = {
@@ -13,36 +14,79 @@ export type channelsData = {
   parent?: string
 };
 
+export type useChannelData = Pick<channelsData,
+  'id' |
+  'channel_name' |
+  'channel_topics' |
+  'channel_type' |
+  'parent'
+  > & {
+  "state": string
+}
+
+const apiChannel: channelsData[] = [
+  {
+    id: "114514",
+    channel_name: "aaa",
+    channel_topics: "aaa is aaa",
+    channel_type: "category",
+    channel_position: 1,
+    creator_id: "810",
+    permissions: ["1"],
+    parent: "1"
+  },
+  {
+    id: "4",
+    channel_name: "aaa",
+    channel_topics: "aaa is aaa",
+    channel_type: "category",
+    channel_position: 1,
+    creator_id: "810",
+    permissions: ["1"],
+    parent: "1"
+  },
+  {
+    id: "1919810",
+    channel_name: "aaa",
+    channel_topics: "aaa is aaa",
+    channel_type: "text",
+    channel_position: 1,
+    creator_id: "810",
+    permissions: ["1"],
+  }
+];
+
 const ChannelList: FC = () => {
 
+  console.log("ChannelLineをレンダー");
 
+  const makeChannels = ():useChannelData[] => {
+    return (
+      apiChannel.map((value) => {
+        return {
+          id: value.id,
+          channel_name: value.channel_name,
+          channel_topics: value.channel_topics,
+          channel_type: value.channel_type,
+          parent: value.parent,
+          state: "open"
+        };
+      })
+    )
+  };
 
-  const apiChannel: channelsData[] = [
-    {
-      id: "114514",
-      channel_name: "aaa",
-      channel_topics: "aaa is aaa",
-      channel_type: "category",
-      channel_position: 1,
-      creator_id: "810",
-      permissions: ["1"],
-      parent: "1"
-    },
-    {
-      id: "1919810",
-      channel_name: "aaa",
-      channel_topics: "aaa is aaa",
-      channel_type: "text",
-      channel_position: 1,
-      creator_id: "810",
-      permissions: ["1"],
-    }
-  ]
+  const [channels, setChannels] = useState<useChannelData[]>(makeChannels);
+
+  useEffect(() => {
+    console.log(apiChannel);
+    setChannels(makeChannels);
+  }, apiChannel);
+
 
   return (
-    <div>
+    <>
       {
-        apiChannel.map((value => {
+        channels.map((value => {
           return (
             <ChannelCard
               key={value.id}
@@ -50,11 +94,12 @@ const ChannelList: FC = () => {
               channel_topics={value.channel_topics}
               channel_type={value.channel_type}
               parent={value.parent}
+              state={value.state}
             />
-          )
+          );
         }))
       }
-    </div>
+    </>
 
   );
 }
