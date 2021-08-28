846e95e1ee37ee53b353a03e5a93cfe0379cf871
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-08-28 16:20:30 +0900

    update sendButton
    PropsのonClickをany型にしてしまった

--- src/components/atoms/sendButton.tsx
+++ src/components/atoms/sendButton.tsx
@@ -1,7 +1,20 @@
 import type { FC } from "react";
 
-import style from "../../styles/components/atoms/sendButton.scss";
+import style from "../../styles/components/atoms/sendButton.module.scss";
 
+// default color #FFDCA8
+type Props = {
+  onClick : any;
+}
 const sendButton: FC<Props> = (props) => {
-
+  return (
+    <button className={style.messageSendButton} onClick={props.onClick}>
+      <svg>
+        <path d="M0.000195503 0L75 28L6.58803 27.5L0.000195503 0Z" />
+        <path d="M0.000183105 56.5L75 28L6.58802 29L0.000183105 56.5Z" />
+      </svg>
+    </button>
+  );
 };
+
+export default sendButton;
