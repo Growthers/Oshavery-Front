6935551ff12d90624e5de3d2406f47919bf16791
Yusuke20s <67366917+Yusuke4869@users.noreply.github.com>
2021-08-29 19:13:19 +0900

    [feat] メンバーカードの実装

--- src/components/atoms/Member.tsx
+++ src/components/atoms/Member.tsx
@@ -1,21 +1,31 @@
 import type { FC } from "react";
 
-import { MembersData } from "../organisms/MemberList";
+import MemberCard from "./MemberCard";
 
 import style from "../../styles/components/atoms/Member.module.scss";
 
-type Props = Pick<
-  MembersData,
-  "name" | "avatar_url" | "bot"
->;
+type Props = {
+  id: string,
+  // element_id: string,
+  name: string,
+  avatar_url: string,
+  bot: boolean,
+  func_show_membercard: (target_id: string) => void
+};
 
-const Member: FC<Props> = props => {
+const Member: FC<Props> = (props) => {
   return (
-    <div className={style.member}>
-      <img className={style.avatar} src={props.avatar_url}></img>
-      {!props.bot && (<span className={style.name}>{props.name}</span>)}
-      {props.bot && (<span className={style.bot_name}>{props.name}</span>)}
-      {props.bot && (<span className={style.bot}>BOT</span>)}
+    <div className={style.member_box}>
+      <div className={`${style.member} member_element`} onClick={() => props.func_show_membercard(props.id)}>
+        <img className={`${style.avatar} member_element`} src={props.avatar_url}></img>
+        {!props.bot && (<span className={`${style.name} member_element`}>{props.name}</span>)}
+        {props.bot && (<span className={`${style.bot_name} member_element`}>{props.name}</span>)}
+        {props.bot && (<span className={`${style.bot} member_element`}>BOT</span>)}
+      </div>
+
+      <div className={style.membercard} id={props.id}>
+        <MemberCard name={props.name} avatar_url={props.avatar_url} bot={props.bot} />
+      </div>
     </div>
   )
 };
--- src/components/organisms/MemberList.tsx
+++ src/components/organisms/MemberList.tsx
@@ -1,25 +1,104 @@
 import type { FC } from "react";
+import { useState } from "react";
 
 import Member from "../atoms/Member";
+
+import member_style from "../../styles/components/atoms/Member.module.scss";
 
 export type MembersData = {
   id: string,
   name: string,
   avatar_url: string,
   bot: boolean
-}
+};
 
 const members_data: MembersData[] = []
 
 const MemberList: FC = () => {
+  const [isShow, setIsShow] = useState(false);
+
+  // メンバーカードのクリア
+  const clear_membercard = () => {
+    if (process.browser) {
+      const elements: HTMLCollectionOf<Element> = document.getElementsByClassName(member_style.membercard);
+
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].className = member_style.membercard;
+      };
+
+      setIsShow(false);
+    };
+  };
+
+  // 引数のIDのクラスを変更（メンバーカード表示）
+  const show_membercard = (target_id: string) => {
+    clear_membercard();
+    if (process.browser) {
+      const target: HTMLElement | null = document.getElementById(target_id);
+
+      if (target == null) {
+        return;
+      };
+
+      target.className = `${member_style.membercard} ${member_style.show}`;
+      setIsShow(true);
+    };
+  };
+
+  // クリックイベント
+  const check_click = (e: any) => {
+    const class_name = String(e.target.className);
+
+    if (class_name.indexOf("member_element") !== -1) {
+      return;
+    };
+
+    if (class_name.indexOf("membercard_element") !== -1) {
+      return;
+    };
+
+    if (!isShow) {
+      return;
+    };
+
+    clear_membercard();
+  };
+
+  if (process.browser) {
+    document.body.onclick = check_click;
+  };
+
+  /*
+  UserId代用のHTML要素ID
+  let element_id_array: string[] = [];
+
+  for (let i = 0; i < members_data.length; i++) {
+    let set_id = "";
+    while (true) {
+      set_id = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+      if (!element_id_array.includes(set_id)) {
+        element_id_array.push(set_id);
+        break;
+      };
+    };
+  };
+  */
+
   return (
     <div>
       {
-        members_data.map((value => {
+        members_data.map((value, index) => {
           return (
-            <Member name={value.name} avatar_url={value.avatar_url} bot={value.bot} />
+            <Member
+              id = {value.id}
+              // element_id={element_id_array[index]}
+              name={value.name}
+              avatar_url={value.avatar_url}
+              bot={value.bot}
+              func_show_membercard={show_membercard}
+            />
           )
-        }))
+        })
       }
     </div>
   )
