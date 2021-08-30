f91b4a483a53a22c5669bdcf1b3380e416afc0ab
Kota Kato <peony.btn@gmail.com>
2021-08-30 16:24:57 +0900

    Function component化

--- src/components/molecules/message.tsx
+++ src/components/molecules/message.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import type { FC } from 'react';
 
 interface Response {
   id: string;
@@ -19,23 +19,21 @@ interface Props {
   response: Response;
 };
 
-class Message extends React.Component<Props, {}> {
-  render() {
-    const r: Response = this.props.response
-    const author_avator: string = r.author.avator
-    const author_name = r.author.name
-    const timestamp = r.timestamp
-    const content = r.content
+const Message: FC<Props> = (props) => {
+  const r: Response = props.response
+  const author_avator: string = r.author.avator
+  const author_name = r.author.name
+  const timestamp = r.timestamp
+  const content = r.content
 
-    return (
-      <div>
-        <img src={author_avator} alt={author_name + "'s avator"}></img>
-        <div>{author_name}</div>
-        <div>{timestamp}</div>
-        <div>{content}</div>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <img src={author_avator} alt={author_name + "'s avator"}></img>
+      <div>{author_name}</div>
+      <div>{timestamp}</div>
+      <div>{content}</div>
+    </div>
+  )
 }
 
 export default Message
