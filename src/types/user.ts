2b3ea76e8e3128e945942e3b1ada5a684650f350
sayoi341 <20alt341@gmail.com>
2021-08-30 23:23:49 +0900

    add: userの型とかAPIのリクエストレスポンスの型を作った

---
+++ src/types/user.ts
@@ -0,0 +1,80 @@
+import {guild} from "./guild";
+
+export type user = {
+  "id": string,
+  "name": string,
+  "avatar": string,
+  "bot": boolean,
+  "state": number
+}
+
+
+// API リクエストレスポンスの型
+
+export type getUserInfoRes = user
+
+//:::::::::::::::::::::::::::::::::::::::::
+
+export type patchUserInfoReq = {
+  "name": string
+}
+
+export type patchUserInfoRes = {
+  "user_id": string,
+  "user_name": string,
+  "user_avatar": string,
+  "bot": boolean
+}
+
+//:::::::::::::::::::::::::::::::::::::::
+
+export type postUserInfoReq = {
+  "name": string,
+  "password": string,
+  "bot": boolean
+}
+
+export type postUserInfoRes = {
+  "id": string,
+  "bot": string,
+  "name": string
+}
+
+//::::::::::::::::::::::::::::::::::::
+
+export type getUsersInfoRes = user[]
+
+//:::::::::::::::::::::::::::::::::::
+
+export type getMyInfoRes = user & guild[]
+
+//::::::::::::::::::::::::::::::::::
+
+export type patchMyInfoReq = {
+  "name": string
+}
+
+//::::::::::::::::::::::::::::::::
+
+export type postCreateBotReq = {
+  "description": string,
+  "name": string
+}
+
+//:::::::::::::::::::::::::::::::
+
+export type getBotListReq = {
+  "id": string,
+  "botUserId": string,
+  "description": string,
+  "owner": {
+    "id": string,
+    "name": string,
+    "avatar": string,
+    "bot": boolean,
+    "state": number
+  },
+  "state": number,
+  "created_at": string,
+  "updated_at": string
+}[]
