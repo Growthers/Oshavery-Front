2316361edd81bbd5ed109c67484714c3f68b5901
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-08-31 11:44:14 +0900

    Button要素からdiv要素に変更
    Clickらしさをつける必要あり

--- src/components/atoms/SendButton.tsx
+++ src/components/atoms/SendButton.tsx
@@ -7,12 +7,10 @@ type Props = {
 
 const SendButton: FC<Props> = (props) => {
   return (
-    <button className={style.messageSendButton} onClick={props.onClick}>
-      <svg>
+      <svg className={style.messageSendButton} onClick={props.onClick}>
         <path d="M0.000195503 0L75 28L6.58803 27.5L0.000195503 0Z" />
         <path d="M0.000183105 56.5L75 28L6.58802 29L0.000183105 56.5Z" />
       </svg>
-    </button>
   );
 };
 
