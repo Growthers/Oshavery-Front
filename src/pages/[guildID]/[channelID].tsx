4b85e9fd503b2ec8b18390cbeafb127cbad486e4
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2021-09-05 17:47:46 +0900

    Inputの配置

--- src/pages/[guildID]/[channelID].tsx
+++ src/pages/[guildID]/[channelID].tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import ServerList from "../../components/organisms/ServerList";
 import ChannelList from "../../components/organisms/ChannelList";
 import MessageList from "../../components/organisms/MessageList";
+import InputMessageBox from "../../components/molecules/InputMessageBox";
 import MemberList from "../../components/organisms/MemberList";
 
 import style from "../../styles/pages/guild-channel.module.scss";
@@ -22,8 +23,11 @@ const Oshavery: NextPage = () => {
         <div className={style.left_side}>
           <ChannelList />
         </div>
-        <div className={style.message_list}>
+        <div className={style.message}>
           <MessageList />
+        </div>
+        <div className={style.input}>
+          <InputMessageBox />
         </div>
         <div className={style.right_side}>
           <MemberList />
