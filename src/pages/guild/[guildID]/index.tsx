cf653f9de8428f6ee553a139af1ff4ea35544811
sayoi341 <20alt341@gmail.com>
2021-09-10 17:43:25 +0900

    change: ディレクトリ構成の変更

---
+++ src/pages/guild/[guildID]/index.tsx
@@ -0,0 +1,25 @@
+import type {NextPage} from 'next'
+import {FC, useContext, useEffect} from "react";
+import {userContext} from "../../../stores/user";
+import {useRouter} from "next/router";
+
+const Child: FC = () => {
+  const router = useRouter()
+  const {guildID} = router.query
+  const {userState} = useContext(userContext)
+
+  useEffect(() => {
+    router.push(
+      "/guild/[guildID]/channel/[channelID]",
+      `/guild/${guildID}/channel/${userState.user.guilds[userState.user.guilds.findIndex(item => item.id === guildID)].channels[0].id}`)
+  })
+
+  return (<>自動的にリダイレクトします</>)
+}
+
+
+const Test: NextPage = () => {
+  return <Child/>
+}
+
+export default Test
