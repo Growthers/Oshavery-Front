b724a8f2555f408c0361ec6144e5f93df3dc257e
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-09-10 19:35:44 +0900

    ポップアップが可能になった(バグあり) focusで取ったせいで、inputで閉じる

--- src/components/molecules/EmojiPicker.tsx
+++ src/components/molecules/EmojiPicker.tsx
@@ -1,26 +1,51 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import "emoji-mart/css/emoji-mart.css";
-import { CustomEmoji, EmojiData, Picker } from "emoji-mart";
+import { CustomEmoji, EmojiData, Picker, Emoji } from "emoji-mart";
+import Popup from "../atoms/Popup";
 
 type EmojiProps = {
-  custom: CustomEmoji[];
-  color: string;
-  onSelect: (emoji: EmojiData) => void;
+  //custom: CustomEmoji[];
+  //color: string;
+  // onSelect: (emoji: EmojiData) => void;
 };
 
 const EmojiPicker: FC<EmojiProps> = (props) => {
+  const [isShow, setIsShow] = useState(false);
+  const popupRef = useRef(null);
+  useEffect(() => {
+    isShow && popupRef.current.focus();
+  }, [isShow]);
+
   return (
-    <Picker
-      title="Pick your emoji…"
-      emoji="point_up"
-      autoFocus={true}
-      emojiSize={33}
-      theme="dark"
-      set="twitter"
-      custom={props.custom}
-      color={props.color}
-      onSelect={(emoji) => props.onSelect(emoji)}
-    />
+    <div
+      onClick={() => {
+        if (!isShow) setIsShow(true);
+      }}
+      ref={popupRef}
+      onBlur={() => setIsShow(false)}
+      tabIndex={1000}
+    >
+      <div
+        onClick={() => {
+          if (!isShow) setIsShow(true);
+        }}
+        hidden={!isShow}
+      >
+        <Picker
+          title="Pick your emoji…"
+          emoji="point_up"
+          autoFocus={true}
+          emojiSize={33}
+          theme="dark"
+          set="twitter"
+          useButton={false}
+          /*custom={props.custom} */
+          /*         color={props.color}*/
+          /*          onSelect={(emoji) => props.onSelect(emoji)}*/
+        />
+      </div>
+      <Emoji emoji={"grinning"} size={30} />
+    </div>
   );
 };
 
