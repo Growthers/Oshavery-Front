1d2e57d15093dadbc9fa975c72454dbb54d0f123
sayoi341 <20alt341@gmail.com>
2021-09-15 03:52:53 +0900

    wip

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -1,26 +1,51 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
-import { FC, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useUser } from '@auth0/nextjs-auth0';
 import { client } from "../lib/client";
+import { useRouter } from "next/router";
+import { userContext } from "../stores/user";
+import { myInfo, user } from "../types/user";
 
-
-const Child: FC = () => {
-  const { user } = useUser();
-
-  useEffect(() => {
-  },[])
-
-
-  return (
-    <h1>Loading</h1>
-  );
-};
 
 const Loading: NextPage = () => {
+  const router = useRouter()
+  const {user} = useUser()
+  const {userDispatch} = useContext(userContext)
+
+  useEffect(() => {
+    //Oshaveryのuidをcookie(auth0)から読み取る
+    const OshaveryUID = "";
+    (async () => {
+      try {
+        //アカウントあり
+        const me = await client.get<user>(`/users/${OshaveryUID}`)
+
+        //JWTまち
+
+        const myInfo = await client.get<myInfo>("/users/me")
+        userDispatch({
+          type: "set",
+          newData: myInfo.data
+        })
+
+
+        await router.push({
+          pathname:"/guild/[guildID]/channel/[channelID]",
+          query: {
+            guildID: myInfo.data.guilds[0].id,
+            channelID: myInfo.data.guilds[0].channels[0].id
+          }
+        })
+      } catch (e) {
+        //アカウント作成ページへとばすなど
+      }
+    })();
+  },[router, userDispatch])
+
   return (
     <div className={styles.container}>
-      <Child />
+      Loading
     </div>
   );
 };
