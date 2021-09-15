2b0f83f2f90cf3841a308b4f2a2f8859b094a38f
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-09-16 00:08:12 +0900

    fix: next.config.js
    fix domain

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-require("dotenv").config();
 module.exports = {
   images: {
-    //    domains: ["media.oshavery-app.net", "storage.googleapis.com"],
-    domains: process.env.IMAGES_DOMAINS.split(' ')
+    domains: [process.env.IMAGES_DOMAINS],
   },
   reactStrictMode: true,
 };
