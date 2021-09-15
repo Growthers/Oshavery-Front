3c188ffadd9d3d8934760012e365f93341942bcc
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-09-15 23:48:19 +0900

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+require("dotenv").config();
 module.exports = {
+  images: {
+    //    domains: ["media.oshavery-app.net", "storage.googleapis.com"],
+    domains: process.env.IMAGES_DOMAINS.split(' ')
+  },
   reactStrictMode: true,
-}
+};
