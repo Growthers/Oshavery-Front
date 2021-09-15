794dbcc194eb1df3bf2ea0a6d97e5d763b26606e
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-09-15 20:52:11 +0900

    delete GuildName.tsx

--- src/app_components/atoms/GuildName.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactNode } from "react";
-import styles from "../../styles/app_components/atoms/GuildName.module.scss";
-type Props = {
-  children: ReactNode;
-};
-
-const GuildName: FC<Props> = (props) => {
-  return <div className={styles.guildName}>{props.children}</div>;
-};
