691eac1dd6d818ddf14c802828b0952964e521b2
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-09-17 18:02:00 +0900

    create UserIcon

--- src/app_components/atoms/UserIcon.tsx
+++ src/app_components/atoms/UserIcon.tsx
@@ -1,22 +1,12 @@
-import { FC, createRef, useState } from "react";
+import { FC } from "react";
 import style from "../../styles/app_components/atoms/UserIcon.module.scss";
 type iconProps = {
-  iconFile: File;
+  imgUrl: string;
+  size: number;
 };
 
 const UserIcon: FC<iconProps> = (props) => {
-  const imgRef = createRef<HTMLImageElement>();
-  const [imgUrl, setImgUrl] = useState("");
-
-  const previewFile = (file: File) => {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      const imgUrl = e.target.result;
-      imgRef;
-    };
-  };
-
-  return <img src={this} className={style.icon} ref={imgRef}></img>;
+  return <img src={props.imgUrl} width={props.size} height={props.size} className={style.icon}></img>;
 };
 
 export default UserIcon;
