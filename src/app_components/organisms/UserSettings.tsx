a5a723401309db4d9e96212491bd4b57255f50a9
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-09-17 23:03:17 +0900

    rename: Settings.tsx -> UserSettings
    名前空間がかぶってた

--- src/app_components/organisms/UserSettings.tsx
+++ src/app_components/organisms/UserSettings.tsx
@@ -2,32 +2,37 @@ import { FC, useState, memo } from "react";
 import InputBox from "../atoms/InputBox";
 import style from "../../styles/app_components/organisms/Settings.module.scss";
 import Modal from "react-modal";
-import UploadButton from "../atoms/UploadButton";
+import IconUploader from "../molecules/IconUploader";
 
 Modal.setAppElement("#__next");
 
-const modalStyle = {
-  content: {
-    position: "abusolute",
-    backgroundColor: "#ffc266",
-  },
-};
 const Settings: FC = () => {
   const [isOpen, setIsopen] = useState(true);
   const [userName, setUserName] = useState("");
+  //  const [icon, setIcon] = useState<File>();
   return (
     <div>
-      <Modal isOpen={isOpen} onRequestClose={() => setIsopen(false)}>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={() => setIsopen(false)}
+        className={`${style.Modal} ${style.Overlay}`}>
         <div className={style.nameAndIcon}>
+          <div className={style.uploadIcon}>
+            <IconUploader />
+          </div>
           <div className={style.userName}>
             <p>ユーザネーム</p>
             <InputBox value={userName} onChange={setUserName} />
           </div>
-
         </div>
+
+        {/* <div className={style.setSNS}>
+          <p>Twitter</p>
+          </div>*/}
         <button onClick={() => setIsopen(false)}>保存</button>
       </Modal>
     </div>
   );
 };
 export default memo(Settings);
+// iconがnullの時、
