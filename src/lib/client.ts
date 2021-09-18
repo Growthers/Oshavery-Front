81705bdc96fcbcab06a38f38d38daa7705f54fcb
sayoi341 <20alt341@gmail.com>
2021-09-19 01:09:25 +0900

    fix

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 export const client: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_ENDPOINT,
+  baseURL: process.env.NEXT_PUBLIC_APIENDPOINT,
   headers: {
     "Content-Type": "application/json",
     "X-Requested-With": "XMLHttpRequest",
