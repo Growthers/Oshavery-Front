20e78f8ef55bfa2559ff870af34c16e93bb2b281
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2021-09-19 09:00:05 +0900

    設定ボタンのスタイル

--- src/app_components/organisms/UserStatus.tsx
+++ src/app_components/organisms/UserStatus.tsx
@@ -22,7 +22,7 @@ const UserStatus: FC<Props> = (props) => {
         <span color="red">error</span>
       )}
       <div className={style.settingbutton}>
-        {true ? <AiOutlineSetting size={38} onClick={() => props.onClick(!props.modalIsShow)} /> : <></>}
+        {true ? <AiOutlineSetting size={30} onClick={() => props.onClick(!props.modalIsShow)} /> : <></>}
       </div>
     </div>
   );
