e98b03a111631069f5b9fc42c78aeb467009dce5
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-10-08 01:51:29 +0900

    fix small error

--- src/app_components/atoms/FileContent.tsx
+++ src/app_components/atoms/FileContent.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import Image from "next/image";
+
 type FileProps = {
   name: string;
   mime: string;
@@ -9,22 +10,25 @@ type FileProps = {
 };
 
 const FileContent: FC<FileProps> = (props) => {
-  if ("image/png" == props.mime || "image/jpeg" == props.mime || "image/gif" == props.mime) {
+  if (props.mime !== null) {
+    return <></>;
+  }
+  if (props.mime === "image/png" || props.mime === "image/jpeg" || props.mime === "image/gif") {
     return (
       <a href={props.media_url} style={{ backgroundColor: "gray" }}>
-        {/*heightの上限値を 決める必要あり widthも同様 css側でどうにかしてください*/}
+        {/* heightの上限値を 決める必要あり widthも同様 css側でどうにかしてください */}
         <Image src={props.media_url} width={props.width} height={props.height} alt={props.name} objectFit="contain" />
       </a>
     );
-  } else if ("video/mp4" == props.mime) {
+  }
+  if (props.mime === "video/mp4") {
     return (
       <video controls width={props.width}>
-        <source src={props.media_url} type={"video/mp4"}></source>
+        <source src={props.media_url} type="video/mp4" />
       </video>
     );
-  } else {
-    return <a href={props.media_url}>{props.name}</a>;
   }
+  return <a href={props.media_url}>{props.name}</a>;
 };
 
 export default React.memo(FileContent);
