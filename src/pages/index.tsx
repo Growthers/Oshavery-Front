04f1d5b93d4efa091535ce714342eb8cd9560098
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-10-10 01:12:07 +0900

    rename: login.tsx => index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import type { NextPage } from "next";
+import Link from "next/link";
+
+const HOME: NextPage = () => {
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  return (
+    <>
+      <h1>Login Page</h1>
+      {isAuthenticated ? (
+        <Link href="/loading">
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a>Go to Oshavery</a>
+        </Link>
+      ) : (
+        <>
+          {/* eslint-disable-next-line jsx-a11y/no-redundant-roles */}
+          <button type="button" role="button" onClick={() => loginWithRedirect()}>
+            Login
+          </button>
+        </>
+      )}
+    </>
+  );
+};
+
+export default HOME;
