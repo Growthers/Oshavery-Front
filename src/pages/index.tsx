468e00c6bb783346f09b0b5ad23bf09e3405fb91
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-10-10 15:12:48 +0900

    create loginPage.module.scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,29 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import type { NextPage } from "next";
+import { useAuth0 } from "@auth0/auth0-react";
 import Link from "next/link";
+import style from "../styles/app_components/organisms/loginPage.module.scss";
 
 const Home: NextPage = () => {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
   return (
-    <>
-      <h1>Login Page</h1>
-      {isAuthenticated ? (
-        <Link href="/loading">
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a>Go to Oshavery</a>
-        </Link>
-      ) : (
-        <>
-          {/* eslint-disable-next-line jsx-a11y/no-redundant-roles */}
-          <button type="button" role="button" onClick={() => loginWithRedirect()}>
-            Login
-          </button>
-        </>
-      )}
-    </>
+    <div className={style.page}>
+      <div className={style.loginPage}>
+        <div>Login Page</div>
+        {isAuthenticated ? (
+          <Link href="/loading">
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a className={style.item}>Go to Oshavery</a>
+          </Link>
+        ) : (
+          <>
+            {/* eslint-disable-next-line jsx-a11y/no-redundant-roles */}
+            <button type="button" role="button" onClick={() => loginWithRedirect()} className={style.item}>
+              Login
+            </button>
+          </>
+        )}
+      </div>
+    </div>
   );
 };
 
