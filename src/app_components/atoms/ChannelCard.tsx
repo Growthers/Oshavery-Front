781b93af275dcd6e6577e6eec2f3a789585d4431
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-10-29 01:29:37 +0900

    fix atoms/ChannelCard.tsx
    I coment outed unimplemented function.

--- src/app_components/atoms/ChannelCard.tsx
+++ src/app_components/atoms/ChannelCard.tsx
@@ -7,7 +7,8 @@ import style from "../../styles/app_components/atoms/ChannelCard.module.scss";
 type Props = {
   channel_name: string;
   channel_type: string;
-  ChannelTopics: string;
+  //  未実装のためコメントアウト
+  // ChannelTopics: string;
   link: string;
   selected: boolean;
 };
@@ -22,6 +23,7 @@ const ChannelCard: FC<Props> = (props) => {
   }
   return (
     <Link href="/guild/[guildID]/channel/[channelID]" as={props.link}>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a tabIndex={0} role="button" className={`${style.channel} ${props.selected && style.selected}`}>
         <FaHashtag className={style.sharp} />
         <span className={style.channel_name}>{props.channel_name}</span>
