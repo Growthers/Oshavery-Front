84912ad4af62f7f19ecc20f9167e3c1f8f9c3ff4
Re-taro <aminokacho@gmail.com>
2022-01-15 00:19:01 +0900

    🔧 update storybook config
    suport other CSS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: ["../src/components/**/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-postcss"],
   framework: "@storybook/react",
 };
